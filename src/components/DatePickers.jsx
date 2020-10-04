672c023d09d94c0dbad772fbbd807d34e939f0c6
Rashmi Sarode <rashmisarode92@gmail.com>
2020-10-03 21:01:31 -0700

    grid and datepicker added

--- src/components/DatePickers.jsx
+++ src/components/DatePickers.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { Container, Button, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -17,28 +18,55 @@ const useStyles = makeStyles((theme) => ({
 export default function DatePickers() {
   const classes = useStyles();
 
+  const [fromDate, setFromDate] = useState("2019-10-01");
+  const [toDate, setToDate] = useState("2019-10-01");
+
+
+  const onSubmit = (event) => {
+    console.log(fromDate, toDate);
+
+  }
+
   return (
-    <form className={classes.container} noValidate>
-      <TextField
-        id="date"
-        label="From Date"
-        type="date"
-        defaultValue="2019-10-1"
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-       <TextField
-        id="date"
-        label="To Date"
-        type="date"
-        defaultValue="2019-10-1"
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-    </form>
+    <Container>
+      <form className={classes.container} noValidate>
+        <Grid container style={{ padding: 30 }}>
+          <Grid item xs={4}>
+            <TextField
+              id="date"
+              label="From Date"
+              type="date"
+              defaultValue={fromDate}
+              onChange={e=> setFromDate(e.target.value)}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              id="date"
+              label="To Date"
+              type="date"
+              defaultValue={toDate}
+              onChange={e=> setToDate(e.target.value)}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+
+          </Grid>
+          <Grid item  xs={4}>
+            <Button variant="contained" size="large" color="primary" onClick={onSubmit}>
+              Submit
+            </Button>
+          </Grid>
+
+        </Grid>
+      </form>
+    </Container>
+
   );
 }
--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,8 +1,20 @@
 import React, { PureComponent } from 'react'
 import CustomNavBar from './NavBar';
 import DatePickers from './DatePickers';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 class MainView extends PureComponent {
     constructor(props) {
         super(props)
@@ -12,17 +24,29 @@ class MainView extends PureComponent {
         }
     }
 
-    
-
     render() {
+        const classes = this.props.classes;
         return (
             <div>
-                <CustomNavBar/>
-                <DatePickers/>
-            
+                <CustomNavBar />
+                <div className={classes.root}>
+                    <Grid container spacing={3} style={{ padding: 20 }} justify="center">
+                        <Grid item xs={6} style={{ padding: 20 }} >
+                            <Paper className={classes.paper}><DatePickers /></Paper>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={3} style={{ padding: 20 }} justify="center">
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>xs=12</Paper>
+                        </Grid>
+                    </Grid>
+                </div>
             </div>
+
         )
     }
 }
 
-export default MainView
+
+export default withStyles(useStyles)(MainView);
+
