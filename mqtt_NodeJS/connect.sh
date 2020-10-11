300fb20010747e82877a5180d7c9c364e0d4773b
Rashmi Sarode <rashmisarode92@gmail.com>
2020-10-10 23:59:00 -0700

    added mqtt nodejs client

---
+++ mqtt_NodeJS/connect.sh
@@ -0,0 +1,9 @@
+ node cloudiot_mqtt_example_nodejs.js \
+    mqttDeviceDemo \
+    --projectId=neural-cable-290804 \
+    --cloudRegion=us-central1 \
+    --registryId=my-registry \
+    --deviceId=my-device \
+    --privateKeyFile=rsa_private.pem \
+    --numMessages=25 \
+    --algorithm=RS256
